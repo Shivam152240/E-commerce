33095aec80b402c323c7c536cfb85993192578b8
Shivam Prajapati <shivamkumar152240@gmail.com>
2026-03-18 22:41:15 +0530

    change register.jsx

--- frontend/src/assets/pages/Register.jsx
+++ frontend/src/assets/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Register.css';
+import './register.css';
 import axios from 'axios';
 
 export default function Register() {
